56d908b05ff38756815d0575b46afcdb899d6378
jd <jean.daniel.michaud@gmail.com>
2021-10-03 12:05:13 +0200

    Dev apps: Allow HTTPS config if certificate file are present

--- dev/canvas-processing/rollup.config.js
+++ dev/canvas-processing/rollup.config.js
@@ -5,6 +5,22 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import serve from 'rollup-plugin-serve';
 import copy from 'rollup-plugin-copy'
+import * as fs from 'fs';
+
+// Boiler plate to check certificate files and activate https if present.
+const serverKeyPath = `${process.env.HOME}/.local/share/ssl/certs/server.key`;
+const serverCertPath = `${process.env.HOME}/.local/share/ssl/certs/server.crt`;
+const caPath = `${process.env.HOME}/.local/share/ssl/certs/ca.pem`;
+
+const serverKey = fs.existsSync(serverKeyPath) || (console.log(`no server.key in ${serverKeyPath}`), false);
+const serverCert = fs.existsSync(serverCertPath) || (console.log(`no server.cert in ${serverCertPath}`), false);
+const ca = fs.existsSync(caPath) || (console.log(`no ca in ${caPath}`), false);
+
+const https = (serverKey && serverCert && ca) ? {
+  key: fs.readFileSync(serverKeyPath),
+  cert: fs.readFileSync(serverCertPath),
+  ca: fs.readFileSync(caPath)
+} : (console.log('https disabled'), undefined);
 
 export default {
   input: 'dev/canvas-processing/main.ts',
@@ -13,6 +29,9 @@ export default {
     file: 'dev/canvas-processing/dist/main.js',
     format: 'umd',
     sourcemap: true,
+  },
+  watch: {
+    clearScreen: false,
   },
   plugins: [
     resolve(),
@@ -32,6 +51,7 @@ export default {
         'Cross-Origin-Opener-Policy': 'same-origin',
         'Cross-Origin-Embedder-Policy': 'require-corp',
       },
+      https,
     }),
   ],
 }
